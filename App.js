8729de893230bf60cdd5922a5a326dec16f041f3
NaelDev1 <naelmorais314@gmail.com>
2023-12-04 19:45:47 -0300

    Making the preloader of the main page

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 var btnContact = document.querySelector('.nd-btn-contact');
 
+window.addEventListener('load', function(){
+      var pagePreloader = document.querySelector('.nd-preloader');
+      
+      setTimeout(function(){
+        pagePreloader.classList.add('nd-fade-out');
+    },2500);
+    setTimeout(function(){
+        pagePreloader.style.display = 'none';
+  },2700);
+});
+
+
 btnContact.addEventListener('click', function(){
     var boxContact = document.querySelector('.nd-contact-info');
     boxContact.classList.toggle('nd-is-open');
     this.classList.toggle('nd-change-icon');
-})
+}); 
